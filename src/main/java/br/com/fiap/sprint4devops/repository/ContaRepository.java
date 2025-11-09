53887114c72b2d2d5f9d7602d5947fe6634cf6e8
calazans-99 <60239479+calazans-99@users.noreply.github.com>
2025-11-09 14:30:13 -0300

    Update ContaRepository.java

--- src/main/java/br/com/fiap/sprint4devops/repository/ContaRepository.java
+++ src/main/java/br/com/fiap/sprint4devops/repository/ContaRepository.java
@@ -1,10 +1,9 @@
 package br.com.fiap.sprint4devops.repository;
 
-import br.com.fiap.sprint4devops.entity.Conta;
+import br.com.fiap.sprint4devops.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface ContaRepository extends JpaRepository<Conta, Long> {
-    Optional<Conta> findByEmail(String email);
 }
