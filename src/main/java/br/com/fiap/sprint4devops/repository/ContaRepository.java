002a3f20a6fd32b6eb96500d666279c850f89456
calazans-99 <60239479+calazans-99@users.noreply.github.com>
2025-11-09 15:50:48 -0300

    Add method to find Conta by email

--- src/main/java/br/com/fiap/sprint4devops/repository/ContaRepository.java
+++ src/main/java/br/com/fiap/sprint4devops/repository/ContaRepository.java
@@ -1,9 +1,10 @@
 package br.com.fiap.sprint4devops.repository;
 
-import br.com.fiap.sprint4devops.model.Conta;
+import br.com.fiap.sprint4devops.entity.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface ContaRepository extends JpaRepository<Conta, Long> {
+    Optional<Conta> findByEmail(String email);
 }
